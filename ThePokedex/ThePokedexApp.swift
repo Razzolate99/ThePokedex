b35672d44364e4372d179a335e8a642bd6032ba2
Aaron Ward <Lord>
2021-08-25 10:04:34 -0400

    Initial Commit

---
+++ ThePokedex/ThePokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePokedexApp.swift
+//  ThePokedex
+//
+//  Created by Aaron Ward on 2021-08-25.
+//
+
+import SwiftUI
+
+@main
+struct ThePokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
